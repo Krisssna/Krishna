22df43a16aa669cc71bb0c311e06a231b6274883
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-08-29 13:42:48 +0545

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const newsLink = document.querySelector('nav ul li a[href="#news"]');
-
-    // Use your Google Apps Script URL here
-    const proxyUrl = "https://script.google.com/macros/s/AKfycbzBomLGMYJNeh2hmt2_y0mStt9NptXUHdDW5mLu3bEa3O75TPQNUzLlsVo6nEMGPPtoTw/exec";
+    const proxyUrl = "https://script.google.com/macros/s/AKfycbzy6mqElWH4GOFQARZ92RNwb5tG69vQKtT0z-_FEwI9sbObTfS8GCXZpMoOJFi-O6V00A/exec";
 
     newsLink.addEventListener('click', function (event) {
         event.preventDefault();
@@ -10,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch(proxyUrl)
             .then(response => response.text())
             .then(data => {
-                // Inject the filtered news content into a news section
                 document.getElementById('news').innerHTML = data;
             })
             .catch(error => console.error('Error fetching the news:', error));
