f9395571614fbb4b2a651bd2c9a51daa695443fa
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-08-29 14:25:36 +0545

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const newsLink = document.querySelector('nav ul li a[href="#news"]');
+    const newsLink = document.querySelector('nav ul li a[href="news"]');
     const proxyUrl = "https://script.google.com/macros/s/AKfycbzy6mqElWH4GOFQARZ92RNwb5tG69vQKtT0z-_FEwI9sbObTfS8GCXZpMoOJFi-O6V00A/exec";
 
     newsLink.addEventListener('click', function (event) {
