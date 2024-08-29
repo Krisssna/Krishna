1c94a6191686f2b8a43603bebddc3967ed6cc71d
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-08-29 13:31:27 +0545

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const newsLink = document.querySelector('nav ul li a[href="#news"]');
 
     // Use your Google Apps Script URL here
-    const proxyUrl = https://script.google.com/macros/s/AKfycbzBomLGMYJNeh2hmt2_y0mStt9NptXUHdDW5mLu3bEa3O75TPQNUzLlsVo6nEMGPPtoTw/exec;
+    const proxyUrl = "https://script.google.com/macros/s/AKfycbzBomLGMYJNeh2hmt2_y0mStt9NptXUHdDW5mLu3bEa3O75TPQNUzLlsVo6nEMGPPtoTw/exec";
 
     newsLink.addEventListener('click', function (event) {
         event.preventDefault();
