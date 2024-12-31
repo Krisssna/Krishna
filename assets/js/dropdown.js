13df7c9c2644ffe7e5b0950bce0269dd95bfa602
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-12-31 19:47:44 +0545

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,1 +1,18 @@
 
+$(document).ready(function() {
+    // Hide all sub-dropdowns initially
+    $('.sub-dropdown-content').hide();
+
+    // Toggle sub-dropdown on click
+    $('.dropdown-content > li > a').on('click', function(e) {
+        e.preventDefault();
+        $(this).next('.sub-dropdown-content').toggle();
+    });
+
+    // Hide sub-dropdown when clicking outside
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.dropdown-content').length) {
+            $('.sub-dropdown-content').hide();
+        }
+    });
+});
