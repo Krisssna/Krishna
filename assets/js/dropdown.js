1307044f5076d8979d6aaa67885373907f562928
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-12-31 21:02:13 +0545

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,15 +1,15 @@
-
 $(document).ready(function() {
     // Hide all sub-dropdowns initially
     $('.sub-dropdown-content').hide();
 
-    // Toggle sub-dropdown on click
-    $('.dropdown-content > li > a').on('click', function(e) {
-        e.preventDefault();
-        $(this).next('.sub-dropdown-content').toggle();
+    // Show sub-dropdown on hover (replace click with hover for showing)
+    $('.dropdown-content > li').hover(function() {
+        $(this).find('.sub-dropdown-content').show();
+    }, function() {
+        $(this).find('.sub-dropdown-content').hide();
     });
 
-    // Hide sub-dropdown when clicking outside
+    // Hide sub-dropdown when clicking outside (optional if you prefer hover)
     $(document).on('click', function(e) {
         if (!$(e.target).closest('.dropdown-content').length) {
             $('.sub-dropdown-content').hide();
