b1b6dbfb3e153eaee895cbe1b54d9f1192de747c
Krisssna <92026573+Krisssna@users.noreply.github.com>
2025-01-02 11:02:59 +0545

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
+
+document.addEventListener('click', function(event) {
+    const menu = document.getElementById('menu');
+    const menuIcon = document.querySelector('.menu-icon');
+    if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+        menu.style.display = 'none';
+    }
+});
+
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    dropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
+        const content = this.querySelector('.dropdown-content');
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const progressBars = document.querySelectorAll(".progress-bar");
+
+    progressBars.forEach((bar) => {
+        const value = bar.getAttribute("aria-valuenow");
+        bar.style.width = value + "%"; // Set the width based on aria-valuenow
+    });
+});
