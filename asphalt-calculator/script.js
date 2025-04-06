cc4d1a1bf8295f649dbd37c8e9a8a92ba491826b
Krisssna <92026573+Krisssna@users.noreply.github.com>
2025-04-06 10:31:52 +0545

    Update script.js

--- asphalt-calculator/script.js
+++ asphalt-calculator/script.js
@@ -7,7 +7,7 @@ function handleBituminousSelection(type) {
     const moreButton = document.getElementById('more-button');
     if (moreButton) {
         moreButton.classList.remove('hidden');
-        moreButton.textContent = 'Read more';
+        moreButton.textContent = `More about ${type}`;
     }
 }
 
@@ -26,7 +26,7 @@ function openMorePopup() {
         'BC-19mm': 'bc.html',
         'PMC-Close': 'pmcc.html',
         'PMC-Open': 'pmc.html',
-        'SandSeal': 'sandasphalt.html'
+        'SandAsphalt': 'sandasphalt.html'
     };
 
     const htmlFile = typeToFileMap[currentType] || 'default.html';
